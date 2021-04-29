fa40460371d30bd3b9bb3a5d2f855fe46280755f
MrValanda <dota.doctor@mail.ru>
2021-04-29 10:35:30 +0300

    Add procedure generation

---
+++ Assets/Scripts/MapGenerator/GenerationMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class GenerationMap : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _levelTemplates;
+    [SerializeField] private int _countRoom;
+    [SerializeField] private GameObject _startTemplate, _endTemplate;
+    private void Start()
+    {
+        Random random = new Random();
+        Transform prevTemplate = Instantiate(_startTemplate, Vector3.zero, Quaternion.identity)
+            .GetComponent<TemplateLevel>().NextLvlPoint;
+       
+        for (int i = 0; i < _countRoom; i++)
+        {
+            var levelTemplate = _levelTemplates[random.Next(0,_levelTemplates.Count)];
+            var instantiate = Instantiate(levelTemplate,prevTemplate.position,Quaternion.identity).GetComponent<TemplateLevel>();
+
+            while (instantiate.StartLvlPoint.rotation != prevTemplate.rotation)
+                instantiate.transform.Rotate(Vector3.up * 1f);
+            
+            prevTemplate=instantiate.GetComponent<TemplateLevel>().NextLvlPoint;
+        }
+
+        Instantiate(_endTemplate, prevTemplate.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/MapGenerator/TemplateLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TemplateLevel : MonoBehaviour
+{
+    [SerializeField] private Transform _nextLvlPoint;
+    [SerializeField] private Transform _startLvlPoint;
+    public Transform NextLvlPoint => _nextLvlPoint;
+
+    public Transform StartLvlPoint => _startLvlPoint;
+}
